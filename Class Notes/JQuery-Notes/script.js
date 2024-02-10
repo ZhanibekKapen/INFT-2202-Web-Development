6aa69f323520e4f6bd1106971f1ba6d7739cc224
Zhanibek Kapen <zhanibek.kapen@durhamcollege.ca>
2024-02-09 20:57:06 -0500

    Lab 1 Added

---
+++ Class Notes/JQuery-Notes/script.js
@@ -0,0 +1,17 @@
+const form = $("form");
+
+console.log(form);
+
+console.log(firstNameInput.value);
+
+$('button#submit-btn').click(function(event){
+    event.preventDefault(); 
+    // console.log($("form"));
+    console.log($(''));
+    console.log($("form")[0].elements);
+
+    const myFormArray = $("form")[0].elements;
+    for (letter input myFormArray){
+
+    }
+})
